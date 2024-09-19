e4bdc06ce42a2cb430c7e662efc4502e606d34fb
2021e41@stu.gifu-nct.ac.jp <2021e41@stu.gifu-nct.ac.jp>
2024-09-19 23:10:48 +0900

    created solverprocess class

---
+++ nitgc_visualizer/nitgc_visualizer/SolverProcess.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+class SolverProcess
+{
+//public:
+//	static void create() {
+//		if (not solver) {
+//			Print << U"Failed to create a process";
+//		}
+//
+//	}
+//private:
+//	ChildProcess solver{ U"solver.exe" };
+};
+
